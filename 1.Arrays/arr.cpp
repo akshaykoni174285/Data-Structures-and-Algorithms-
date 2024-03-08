5e3d9c14ac27017c66b4ae46eba031bd209c7680
akshaykoni174285 <akshaykoni174285@gmail.com>
2024-03-08 15:23:31 +0530

    updated on 8 march

--- 1.Arrays/arr.cpp
+++ 1.Arrays/arr.cpp
@@ -1,11 +1,17 @@
 
 #include <iostream>
+#include <vector>
+
 
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    int arr[5] = {1,2,3,4,5};
+    int arr[5] = {1,3,4,5};
+    int arr_size = 4;
+    int pos = 1;
+    int value = 2;
+
     // 1,3,4,5
     //? traversing the array
     // for (size_t i = 0; i < 5; i++)
@@ -14,34 +20,19 @@ int main(int argc, char const *argv[])
     // }
     
     // ? inserting the val in pos 
-    // int pos = 1;
-    // int value = 5;
-    // int length = 4;
-    int arr_len = 5;
 
-    // if (length<arr_len){
-    //     for (size_t i = length; i > pos; i--)
-    //     {
-    //         arr[i] = arr[i-1];
-
-    //     }
-    //     arr[pos] = value;
-    // }
-    // else {
-    //     cout<<"no change"<<endl;
-    // }
-    // ? deletion in array
-    // int del_ind = 1;
-
-    // for (size_t i = del_ind; i < arr_len-1; i++)
-    // {
-    //     arr[i] = arr[i+1];
-
-    // }
+    for (size_t i = arr_size-1; i > pos; i--)
+    {
+        arr[i+1] = arr[i];
+    }
+    arr[pos] = value;
 
     
 
-    for (size_t i = 0; i < arr_len-1; i++)
+   
+    
+
+    for (int i = 0; i < arr_size+1; i++)
     {
         cout<<arr[i]<<endl;
     }
