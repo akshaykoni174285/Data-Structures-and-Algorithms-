13577eec44006fc6b76a8903c5f51f8e16766acb
akshaykoni174285 <akshaykoni174285@gmail.com>
2024-05-28 01:09:22 +0530

    updated on 28 may

---
+++ 9.Trees/BinaryTrees/inorder.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int key;
+    Node* right;
+    Node* left;
+    Node(int val){
+        this->key = val;
+        right = NULL;
+        left = NULL;
+    }
+};
+
+void TraverseBT(Node* root){
+    if(root == NULL) return;
+    TraverseBT(root->left);
+    
+    cout<<root->key;
+    
+    TraverseBT(root->right);
+    
+        
+    
+}
+
+int main(int argc, char const *argv[])
+{
+    Node* root = new Node(0);
+    root->left = new Node(1);
+    root->right = new Node(2);
+    root->left->left = new Node(3);
+    root->left->right = new Node(4);
+    root->right->left = new Node(5);
+    root->right->right = new Node(6);
+    TraverseBT(root);
+
+    return 0;
+}
+
