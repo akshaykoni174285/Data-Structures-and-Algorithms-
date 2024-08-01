fd7cbde3939f7385395fd8673248eca20fbd9979
akshaykoni174285 <akshaykoni174285@gmail.com>
2024-08-01 22:10:59 +0530

    1 aug

--- 5.LinkedList/SingleLL/328_LC_oddnevenLL.cpp
+++ 5.LinkedList/SingleLL/328_LC_oddnevenLL.cpp
@@ -38,20 +38,22 @@ void oddneven(Node* &head){
 
     }    
     Node* odd_ptr = head;
-    Node* even_ptr= head->next;
+    Node* even_ptr= head;
+    even_ptr = even_ptr->next;
+    cout<<even_ptr->data;
 
-    // while(odd_ptr !=nullptr ){
-    //     cout<<odd_ptr->data<<endl;
-    //     odd_ptr = odd_ptr->next->next;
-    // }
-    // while(even_ptr !=nullptr){
-    //     cout<<even_ptr->data<<endl;
-    //     even_ptr = even_ptr->next->next;
-    // }
-    while (odd_ptr!=nullptr || even_ptr!=nullptr)
-    {
-           /* code */
+    while(odd_ptr !=nullptr || even_ptr->next !=nullptr){
+        // cout<<odd_ptr->data<<endl;
+        odd_ptr = odd_ptr->next->next;
     }
+    while(even_ptr !=nullptr){
+        cout<<even_ptr->data<<endl;
+        even_ptr = even_ptr->next->next;
+    }
+    // while (odd_ptr!=nullptr || even_ptr!=nullptr)
+    // {
+    //        /* code */
+    // }
     
 
 
