8f3f57712500f98a67967b776d257b3f774895f1
akshaykoni174285 <akshaykoni174285@gmail.com>
2024-08-16 21:23:02 +0530

    16 aug

--- 1.Arrays/238.movezeros.cpp
+++ 1.Arrays/238.movezeros.cpp
@@ -7,23 +7,21 @@ int main(int argc, char const *argv[])
 {
     vector<int> nums = {0,1,0,3,12};
     int count = 0;
+
     for (size_t i = 0; i < nums.size(); i++)
     {
-        if(nums[i] != 0){
-            swap(nums[i],nums[count]);
+        /* code */
+        if(nums[i]!=0){
+            swap(nums[i], nums[count]);
             count++;
         }
-        
-        /* code */
     }
 
-    // for(int x:nums){
-    //     cout<<x<<endl;
-    // }
-    for (size_t i = 0; i < nums.size(); i++)
-    {
-        cout<<nums[i];
+    for (size_t i = 0; i < nums.size(); i++){
+        cout<<nums[i]<<endl;
     }
+    
+
     
     
     
