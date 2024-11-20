cbc18a184e65c6feb146c4213b1de7b4fb47673a
akshaykoni174285 <akshaykoni174285@gmail.com>
2024-11-20 21:08:51 +0530

    arrays

--- Striver/array/easy/largest.cpp
+++ Striver/array/easy/largest.cpp
@@ -1,21 +1,36 @@
 #include <iostream>
+#include <climits>
 
 using namespace std;
 
 
 int main(int argc, char const *argv[])
 {
-    int arr[] = {1,2,3,4,5,4,3,2,1};
-    int greatest  = arr[0];
-    int arr_size = sizeof(arr)/sizeof(arr[0]);
+    int arr[] = {4,5,1,2,3};
+    int greatest = INT_MIN;
+    int second_greatest = INT_MIN;  
+    int size_arr = sizeof(arr)/sizeof(arr[0]);
 
-    for (size_t i = 1; i < arr_size; i++)
+    if(size_arr<2){
+        cout<<"not enought values to check"<<endl;
+    }
+    for (size_t i = 0; i < size_arr; i++)
     {
         if(arr[i] > greatest){
+            second_greatest = greatest;
             greatest = arr[i];
         }
+        else if(arr[i] > second_greatest && arr[i]!=greatest){
+            second_greatest = arr[i];
+        }
+
     }
-    cout<<greatest<<endl;
+
+    cout<<"second gratest value is "<<second_greatest<<endl;
+
+
+
+    
     
     return 0;
 }
