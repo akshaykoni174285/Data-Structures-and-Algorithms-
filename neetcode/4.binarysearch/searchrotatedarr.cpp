1f76a229b96af97f750f121f3b646dab89987bb6
akshaykoni174285 <akshaykoni174285@gmail.com>
2025-02-21 01:53:04 +0530

    21th feb

---
+++ neetcode/4.binarysearch/searchrotatedarr.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    vector<int> nums = {3,4,5,6,1,2};
+    int target=6;
+
+    int l=0;
+    int r = nums.size()-1;
+
+    while(l<=r){
+        int mid = (l+r) /2;
+
+        if(target == nums[mid]) cout<<mid;
+        if(nums[l]<=nums[mid]){
+            if ( target < nums[l] || target > nums[mid] ){
+                l = mid +1;
+            }
+            else{
+                r = mid -1;;
+            }
+        }
+        else{
+            if(target > nums[r] || target < nums[mid]){
+                r = mid-1;
+
+            }
+            else{
+                l = mid +1;
+            }
+        }
+
+
+    }
+
+    return 0;
+}
