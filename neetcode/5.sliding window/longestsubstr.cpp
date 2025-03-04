24d3fff29f70caebd19c398c5cb49ce8c54981a6
akshaykoni174285 <akshaykoni174285@gmail.com>
2025-03-04 19:55:45 +0530

    4th march

--- neetcode/5.sliding window/longestsubstr.cpp
+++ neetcode/5.sliding window/longestsubstr.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <unordered_set>
+
 
 using namespace std;
 
@@ -8,19 +10,24 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     string s = "ywwkew";
-    int res_max = 0;
-    int point = 0;
-    for (int i = 1; i < s.length(); i++)
+    int left = 0;
+    int max_len = 0;
+    unordered_set<char> myset;
+
+    for (int right = 0; right < s.length(); right++)
     {
-        if(s[point] != s[i]){
-            res_max = max((i-point) + 1,res_max);
+        // if it doesnt find anything it will return end so if duplicate it should not return end
+
+        while(myset.find(s[right])!= myset.end()){
+            myset.erase(s[left]);
+            left++;
 
         }
-        else{
-
-        }
+        myset.insert(s[right]);
+        max_len = max(max_len,right-left+1);
     }
     
+    cout<<"the max string len is :"<<max_len<<endl;
 
     return 0;
 }
