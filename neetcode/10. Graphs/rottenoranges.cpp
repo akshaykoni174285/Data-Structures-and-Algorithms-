edeccac86d8e168d65af84b1016ce66e0d4844cc
akshaykoni174285 <akshaykoni174285@gmail.com>
2025-06-08 22:30:34 +0530

    8 june

--- neetcode/10. Graphs/rottenoranges.cpp
+++ neetcode/10. Graphs/rottenoranges.cpp
@@ -7,8 +7,58 @@ using namespace std;
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
+        vector<vector<bool>> visited(grid.size(),vector<bool>(grid[0].size(),false));
+
+        if(grid.empty()) return 0;
+        int rows = grid.size();
+        int cols = grid[0].size();
+
+        queue<pair<int,int>> q;
+        int fresh = 0;
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(grid[i][j]==2){
+                    q.push({i,j});
+                    visited[i][j] = true;
+                }
+                else if(grid[i][j]==1) fresh++;
+            }
+        }
+
+        if(fresh==0) return 0;
+
+        int minutes = 0;
+        vector<int> dx = {0,0,1,-1};
+        vector<int> dy = {1,-1,0,0};
+
+        while(!q.empty()){
+            int size = q.size();
+            for(int i=0;i<size;i++){
+                auto [x,y] = q.front();
+                q.pop();
+
+                for(int k=0;k<4;k++){
+                    int newX = x + dx[k];
+                    int newY = y + dy[k];
+
+                    if(newX>=0 && newX<rows && newY>=0 && newY<cols && grid[newX][newY]==1 && !visited[newX][newY]){
+                        q.push({newX,newY});
+                        visited[newX][newY] = true;
+                        fresh--;
+                    }
+                }
+            }
+            minutes++;
+        }
+        if(fresh>0) return -1;
+        return minutes;
+        
+
+
+
         // You can implement your BFS logic here
         return 0;
+        
     }
 };
 
@@ -22,6 +72,6 @@ int main() {
 
     int result = sol.orangesRotting(grid);
     cout << "Minimum minutes to rot all oranges: " << result << endl;
-
+    cout<<endl;
     return 0;
 }
