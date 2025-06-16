a2570a3b7d7783db3fa1c286f1435e0ecdf1385c
akshaykoni174285 <akshaykoni174285@gmail.com>
2025-06-16 23:03:16 +0530

    16 june

--- neetcode/10. Graphs/rottenoranges.cpp
+++ neetcode/10. Graphs/rottenoranges.cpp
@@ -7,57 +7,55 @@ using namespace std;
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        vector<vector<bool>> visited(grid.size(),vector<bool>(grid[0].size(),false));
-
-        if(grid.empty()) return 0;
+        if(grid.empty()) return -1;
         int rows = grid.size();
         int cols = grid[0].size();
+        // cout<<grid[][]
 
-        queue<pair<int,int>> q;
+        vector<vector<bool>> visited(rows, vector<bool>(cols, false));
+        queue<pair<int, int>> q;
         int fresh = 0;
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-                if(grid[i][j]==2){
-                    q.push({i,j});
+        int totalminutes = 0;
+        vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<cols; j++){
+                if(grid[i][j] == 1 ){
+                    fresh++;
+                }
+                if(grid[i][j] == 2 && !visited[i][j]){
+                    q.push({i, j});
                     visited[i][j] = true;
                 }
-                else if(grid[i][j]==1) fresh++;
             }
         }
 
-        if(fresh==0) return 0;
-
-        int minutes = 0;
-        vector<int> dx = {0,0,1,-1};
-        vector<int> dy = {1,-1,0,0};
-
-        while(!q.empty()){
+        while (!q.empty() && fresh > 0)
+        {
             int size = q.size();
+            totalminutes++;
             for(int i=0;i<size;i++){
-                auto [x,y] = q.front();
+                auto [X,Y] = q.front();
                 q.pop();
-
-                for(int k=0;k<4;k++){
-                    int newX = x + dx[k];
-                    int newY = y + dy[k];
-
-                    if(newX>=0 && newX<rows && newY>=0 && newY<cols && grid[newX][newY]==1 && !visited[newX][newY]){
-                        q.push({newX,newY});
+                for(auto [x,y]:directions){
+                    int newX = X + x;
+                    int newY = Y + y;
+                    if(newX >= 0 && newX < rows && newY >= 0 && newY < cols && grid[newX][newY] == 1 && !visited[newX][newY]){
+                        q.push({newX, newY});
                         visited[newX][newY] = true;
                         fresh--;
                     }
                 }
+
             }
-            minutes++;
         }
-        if(fresh>0) return -1;
-        return minutes;
-        
+        if(fresh != 0){
+            return -1;
+        }
+        else {
+            return totalminutes;
+        }
 
-
-
-        // You can implement your BFS logic here
-        return 0;
         
     }
 };
